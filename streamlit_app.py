af174fce57b3bde2ccfef370e14c0c423ecda4d1
vincenth38 <vincent.heloin@gmail.com>
2021-10-31 09:04:28 -0700

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+
+st.title("Hello world!")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.write(dataframe)
