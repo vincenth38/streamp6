bd939f5d3a0f4ccc20b1f34b0532519608f707c1
vincenth38 <vincent.heloin@gmail.com>
2021-10-31 22:02:20 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,38 @@
 import pandas as pd
 import streamlit as st
+from gsheetsdb import connect
+
 
 st.title("Convert P6")
 #Insert a file uploader that accepts multiple files at a time:
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 # for uploaded_file in uploaded_files:
 #   bytes_data = uploaded_file.read()
 #   st.write("filename:", uploaded_file.name)
 #   st.write(bytes_data)
 
 
-if uploaded_files is not None:
-  df = pd.read_csv(uploaded_files)
-  st.write(df)
+# if uploaded_files is not None:
+#   df = pd.read_csv(uploaded_files)
+#   st.write(df)
+
+
+
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
+
+
