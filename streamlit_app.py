cec898162d35d0fe05a057df740a30b64d2b8b9d
vincenth38 <vincent.heloin@gmail.com>
2021-10-31 17:35:45 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Hello world!")
+st.title("Convert P6")
+#Insert a file uploader that accepts multiple files at a time:
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# for uploaded_file in uploaded_files:
+#   bytes_data = uploaded_file.read()
+#   st.write("filename:", uploaded_file.name)
+#   st.write(bytes_data)
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
+
+if uploaded_files is not None:
+  df = pd.read_csv(uploaded_files)
   st.write(df)
